91d7a85ef02b4fb0089ed8814b21ffb99169cfe0
ericlugo <ericlugo@users.noreply.github.com>
2019-09-01 21:57:13 -0400

    REGISTRATION COMPLETE. on to login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const db = require('../database/dbConfig.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-router.post('/register', (req, res) => {
-  // implement registration
+const secrets = require('../config/secrets.js');
+
+router.post('/register', async (req, res) => {
+  const user = req.body;
+  (!user.username || !user.password) &&
+    res.status(400).json({
+      success: false,
+      message: `Registration must contain a valid username and password`,
+    });
+
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  try {
+    const newUser = await addUser(user);
+    newUser &&
+      res.status(201).json({
+        success: true,
+        message: `User successfully created.`,
+        newUser,
+      });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: `Fatal Error.\n${err}`,
+    });
+  }
 });
 
 router.post('/login', (req, res) => {
@@ -9,3 +37,16 @@ router.post('/login', (req, res) => {
 });
 
 module.exports = router;
+
+async function addUser(user) {
+  const [id] = await db('users').insert(user, 'id');
+  return db('users')
+    .where('id', id)
+    .first();
+}
+
+function findUser(username) {
+  return db('users')
+    .where('username', username)
+    .first();
+}
